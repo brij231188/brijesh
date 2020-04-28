77192c3ac844ebaa4f5dca1aaff7b5193bc36277
root <root@ip-172-31-81-232.ec2.internal>
2020-04-28 11:26:39 +0000

    cahges

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Brijesh
+                    Vinay Sing Rajput
                 </div>
 
                 <div class="links">
