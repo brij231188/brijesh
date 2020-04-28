180f1d3127f01a88a74ab50aaabbeb419c1fa78d
root <root@ip-172-31-81-232.ec2.internal>
2020-04-28 11:20:36 +0000

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Brijesh
                 </div>
 
                 <div class="links">
